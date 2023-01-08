231ba0ebe5d90cc464f313480abff191ea0ccad9
Alex Ciocartau-EXT <alex.ciocartau-ext@continental-corporation.com>
2023-01-08 19:10:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# QtAppBase
+
+Base Qt classes to be used within Qt applications.
+
+Qt5 and Qt6 are supported.
+
+To run cmake, make sure path to Qt is provided by providing passing `-DCMAKE_PREFIX_PATH=C:/Qt/6.3.1/mingw_64/lib/cmake_t` when running cmake.
+
+This repo is still a work in progress...
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <QApplication>
+#include <QDebug>
+#include <QtAppBase/Logger.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+QCoreApplication* createApplication(int &argc, char *argv[])
+{
+    for (int i = 1; i < argc; ++i) {
+        if (!qstrcmp(argv[i], "--nogui"))
+            return new QCoreApplication(argc, argv);
+    }
+    return new QApplication(argc, argv);
+}
+
+int main(int argc, char *argv[])
+{
+    QScopedPointer<QCoreApplication> app(createApplication(argc, argv));
+
+    // Show debug info when app is executed from console
+    #ifdef _WIN32
+        // https://stackoverflow.com/questions/3360548/console-output-in-a-qt-gui-app
+        if (AttachConsole(ATTACH_PARENT_PROCESS))
+        {
+            freopen("CONOUT$", "w", stdout);
+            freopen("CONOUT$", "w", stderr);
+        }
+    #endif
+
+
+    if (qobject_cast<QApplication *>(app.data()))
+    {
+        base::setup_logger(false);
+        base::logger->info("Started application in GUI mode (use flag --nogui to launch in console mode)");
+        // MainWindow w;
+        // w.show();
+        return app->exec();
+    }
+    else
+    {
+        base::setup_logger(true);
+        base::logger->info("Start application in console mode");
+        return app->exec();
+    }
+}
